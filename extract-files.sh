be7b15618dd217df9ad4ef79b254a5e2fe12967d
mizukanaAlt <rina301087@gmail.com>
2026-01-16 11:15:53 +0000

    vince: libmmcamera_ppeiscore add needed libui_shim

--- extract-files.sh
+++ extract-files.sh
@@ -20,6 +20,9 @@ function blob_fixup() {
         vendor/lib/libvidhance_gyro.so)
             "${PATCHELF}" --replace-needed "android.frameworks.sensorservice@1.0.so" "android.frameworks.sensorservice@1.0-v27.so" "${2}"
             ;;
+        vendor/lib/libmmcamera_ppeiscore.so)
+	    "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+	        ;;
         vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
             "${PATCHELF_0_8}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
             ;;
